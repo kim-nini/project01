7cc89144628f7b6b161aec34caf1e86b4b04c1dd
Jiyeon <duddjektjtro@naver.com>
2024-01-03 01:30:51 +0900

    ì게시판 업ë등C,U 취소시 목록으로 돌아가기, QNA 카테고리 선택 수정

--- src/main/resources/static/js/qna-write.js
+++ src/main/resources/static/js/qna-write.js
@@ -1,41 +1,53 @@
+const qnaCate = document.querySelector(".qnaCategory");
+const mainCate = document.querySelector(".mainCategory");
+const subCategory = document.querySelector(".subCategory");
+// 메인 카테고리 select box에 change 이벤트 추가
+mainCate.addEventListener("change", selectCategory);
 
-    const mainCate = document.querySelector(".mainCategory");
-    const subCategory = document.querySelector(".subCategory");
+// qnaCate 선택시 적용되는 함수
+function toggleCategories() {
 
-    // 메인 카테고리 select box에 change 이벤트 추가
-    mainCate.addEventListener("change", selectCategory);
+    // '상품문의'가 선택된 경우에만 mainCategory와 subCategory를 보이도록 설정
+    if (qnaCate.value === '상품문의') {
+        mainCate.style.display = 'block';
+        subCategory.style.display = 'block';
+    } else {
+        mainCate.style.display = 'none';
+        subCategory.style.display = 'none';
+    }
+}
 
 
-    // db에서 불러온 서브 카테고리 화면에 추가해주기
-    // option 동적 생성
-   async function selectCategory(event){
-       subCategory.innerHTML = "";
+// db에서 불러온 서브 카테고리 화면에 추가해주기
+// option 동적 생성
+async function selectCategory(event) {
+    subCategory.innerHTML = "";
 
-       let array  = await categoryRequest();
+    let array = await categoryRequest();
 
-        for (category  of  array) {
-            // console.log(category.cateName);
-            const option = document.createElement('option');
-            option.value = category.cateCode;
-            option.textContent = category.cateName;
-            subCategory.appendChild(option);
-        }
-
-       // subCategory.value.innerHTML = `${category.cateName}`;
-       //  array.forEach(category => {
-       //     console.log(category.cateName);
-       // });
+    for (category of array) {
+        // console.log(category.cateName);
+        const option = document.createElement('option');
+        option.value = category.cateCode;
+        option.textContent = category.cateName;
+        subCategory.appendChild(option);
     }
 
-    // 상세 카테고리이름 불러오기
-   function categoryRequest() {
-        const category = mainCate.options[mainCate.selectedIndex].value;
-        const url =`/grrreung/sub-category?category=${category}`;
-        return fetch(url)
-            .then((response) => {
-                return response.json();
-            });
-   }
+    // subCategory.value.innerHTML = `${category.cateName}`;
+    //  array.forEach(category => {
+    //     console.log(category.cateName);
+    // });
+}
+
+// 상세 카테고리이름 불러오기
+function categoryRequest() {
+    const category = mainCate.options[mainCate.selectedIndex].value;
+    const url = `/grrreung/sub-category?category=${category}`;
+    return fetch(url)
+        .then((response) => {
+            return response.json();
+        });
+}
 
 
 
